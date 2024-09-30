0bc4454a7af4e853cfaa0e4c0ab546b0d30f8f11
Link <1316341442@qq.com>
2024-09-30 22:10:13 +0800

    重构

--- js/Snackbar-M1.js
+++ js/Snackbar-M1.js
@@ -1,31 +1,95 @@
+var snackbar = document.getElementById("snackbar");
+var CloseButton = document.getElementById("CloseButton");
+//var ButtonID3 = document.getElementById("3");
+
+
+function snackbarX() {
+setTimeout(function() {
+  //关闭时的回调函数
+
+}
+, 500);
+}
+
+function clickSnackbar(){
+//按钮点击事件
+ }
+
+function clearSnackbar() {
+setTimeout(function() {
+  if (snackbar.className === "hide") {
+    snackbar.style.display ="none";
+  }
+}
+, 400);
+// 5.4秒后执行此操作
+}
+
 function showSnackbar() {
-
-  var snackbar = document.getElementById("snackbar");
-
+  let snackbarCallback = true;
   // 添加点击其他地方隐藏 snackbar 的功能
-  document.addEventListener("click", function(event) {
-    if (event.target !== snackbar) {
-      snackbar.className = "hide";
-    }
-  });
+  requestAnimationFrame(function() {
+    document.addEventListener("click", function(event) {
+      if (event.target !== snackbar && snackbar.style.display !== "none") {
+    closeSnackbar();
+      }
+    },
+    );
+  }
+)
+// 清除之前的延时函数
+clearTimeout(snackbar.timeoutId);
 
-  // 清除之前的延时函数
+function closeSnackbar(){
+  snackbar.className ="hide";
+  //判断函数没被调用过防止多次调用
+  if (snackbarCallback === true) {
+    clearSnackbar();
+    snackbarX();
+  }
+  //设置false标志函数已经被调用
+  snackbarCallback = false;
   clearTimeout(snackbar.timeoutId);
+}
 
-  // 等待 snackbar 动画完成
-  setTimeout(function() {
-    // 隐藏现有的 snackbar
-    if (snackbar.className === "show") {
-      snackbar.className = "hide";
-    }
+// 延时让snackbar消失动画完成后再隐藏
+setTimeout(function() {
+  // 隐藏现有的 snackbar
+  if (snackbar.className === "show") {
+    snackbar.className ="hide";
+  }
 
   // 将 snacktext 的值填充到元素中
+
+  if (closeText === "") {
+  // 没有指定按钮
+  snackbar.style.padding ="16px";
   snackbar.innerHTML = snacktext;
 
-    // 显示新的 snackbar
-    snackbar.className = "show";
 
-    // 设置一个新的延时函数
-    snackbar.timeoutId = setTimeout(function(){ snackbar.className = "hide"; }, 5000);
-  }, 240); // 调整此值以匹配 snackbar 动画的持续时间
+} else {
+  // 指定了按钮
+  snackbar.style.padding ="16px 64px 16px 16px";
+    snackbar.innerHTML = snacktext + " <span onclick=\"clickSnackbar()\" id=\"CloseButton\">" + closeText + "</span>";
+}
+
+  // 显示新的 snackbar
+  snackbar.className ="show";
+  snackbar.style.display ="block";
+
+  // 设置一个新的延时函数
+  snackbar.timeoutId = setTimeout(function() {
+    snackbar.className ="hide";
+
+    setTimeout(function() {
+      snackbar.style.display ="none";
+if (snackbarCallback === true) {
+      snackbarX();
+      }
+    }
+    , 500);//5.5秒后清除div
+  }
+  , 5000);//5秒后消失
+}
+, 250);// 调整此值以匹配 snackbar 消失动画的持续时间
 }
--- js/Snackbar-M2.js
+++ js/Snackbar-M2.js
@@ -1,45 +1,103 @@
-if (snackbar.className === "hide") {
-      setTimeout(function() {
-    snackbar.style.display = "none";
-  }, 500); // 5.2秒后执行此操作
-    }
+var snackbar = document.getElementById("snackbar");
+var CloseButton = document.getElementById("CloseButton");
+
+
+function snackbarX() {
+setTimeout(function() {
+  //关闭时的回调函数
+if (btn === "3") {
+    alert("关闭了Snackbar")
+  }
+
+}
+, 500);
+}
+
+function clickSnackbar(){
+ alert("你点击了按钮");
+ }
+
+
+
+function clearSnackbar() {
+setTimeout(function() {
+  if (snackbar.className === "hide") {
+    snackbar.style.display ="none";
+  }
+}
+, 500);
+// 5.2秒后执行此操作
+}
+
 
 function showSnackbar() {
-
-  var snackbar = document.getElementById("snackbar");
-
+  let snackbarCallback = true;
   // 添加点击其他地方隐藏 snackbar 的功能
-requestAnimationFrame(function() {
-document.addEventListener("click", function(event) {
-    if (event.target !== snackbar) {
-      snackbar.className = "hide";
-      clearSnackbar();
-    }
-  });
- })
-  // 清除之前的延时函数
+  requestAnimationFrame(function() {
+    document.addEventListener("click", function(event) {
+      if (event.target !== snackbar && snackbar.style.display !== "none") {
+    closeSnackbar();
+    //snackbarCallback = false;
+      }
+    }, //{ once: true }
+    );
+  }
+)
+
+// 清除之前的延时函数
+clearTimeout(snackbar.timeoutId);
+
+function closeSnackbar(){
+  snackbar.className ="hide";
+  //判断函数没被调用过防止多次调用
+  if (snackbarCallback === true) {
+    snackbarX();
+    clearSnackbar();
+  }
+
+  snackbarCallback = false;
   clearTimeout(snackbar.timeoutId);
 
-  // 等待 snackbar 动画完成
-  setTimeout(function() {
-    // 隐藏现有的 snackbar
-    if (snackbar.className === "show") {
-      snackbar.className = "hide";
-    }
+}
+
+// 延时让snackbar消失动画完成后再隐藏
+setTimeout(function() {
+  // 隐藏现有的 snackbar
+  if (snackbar.className === "show") {
+    snackbar.className ="hide";
+  }
 
   // 将 snacktext 的值填充到元素中
+
+  if (closeText === "") {
+  // 没有指定按钮
+  snackbar.style.padding ="16px";
   snackbar.innerHTML = snacktext;
 
-    // 显示新的 snackbar
-    snackbar.className = "show";
-    snackbar.style.display = "block";
 
-    // 设置一个新的延时函数
-    snackbar.timeoutId = setTimeout(function() {
-  snackbar.className = "hide";
-  setTimeout(function() {
-    snackbar.style.display = "none";
-  }, 500); // 5.2秒后执行此操作
-}, 5000);
-  }, 240); // 调整此值以匹配 snackbar 动画的持续时间
+} else {
+  // 指定了按钮
+  snackbar.style.padding ="16px 64px 16px 16px";
+    snackbar.innerHTML = snacktext + " <span onclick=\"clickSnackbar()\" id=\"CloseButton\">" + closeText + "</span>";
+}
+
+  // 显示新的 snackbar
+  snackbar.className ="show";
+  snackbar.style.display ="block";
+
+  // 设置一个新的延时函数
+  snackbar.timeoutId = setTimeout(function() {
+    snackbar.className ="hide";
+    //if (snackbarCallback === true) {
+      snackbarX();
+     // }
+    setTimeout(function() {
+      snackbar.style.display ="none";
+
+    }
+    , 500);//5.5秒后清除div
+  }
+  , 5000);//5秒后消失
+}
+, 240);// 调整此值以匹配 snackbar 消失动画的持续时间
 }
